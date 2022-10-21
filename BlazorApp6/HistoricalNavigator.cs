b044a30a730228204e0c74356440d4d746dd80d1
jtheisen <jens.theisen@github.com>
2022-10-21 22:20:21 +0200

    Commit the version intercepting all a-clicks

---
+++ BlazorApp6/HistoricalNavigator.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
+using System.ComponentModel;
+
+namespace BlazorApp6;
+
+public class HistoricalNavigator : INotifyPropertyChanged
+{
+    private readonly IJSRuntime js;
+    private readonly NavigationManager navigationManager;
+
+    Int32 currentIndex = 0;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public Int32 CurrentPageIndex => currentIndex;
+
+    public HistoricalNavigator(IJSRuntime js, NavigationManager navigationManager)
+    {
+        this.js = js;
+        this.navigationManager = navigationManager;
+
+        navigationManager.RegisterLocationChangingHandler(HandleLocationChanging);
+        navigationManager.LocationChanged += HandleLocationChanged;
+    }
+
+    private ValueTask HandleLocationChanging(LocationChangingContext ctx)
+    {
+        if (Int32.TryParse(ctx.HistoryEntryState, out currentIndex))
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(""));
+        }
+
+        return ValueTask.CompletedTask;
+    }
+
+    private void HandleLocationChanged(Object? _, LocationChangedEventArgs e)
+    {
+        if (Int32.TryParse(e.HistoryEntryState, out currentIndex))
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(""));
+        }
+    }
+
+    [JSInvokable]
+    public void Navigate(String url, Boolean replace)
+    {
+        var options = new NavigationOptions
+        {
+            HistoryEntryState = (replace ? currentIndex : currentIndex + 1).ToString(),
+            ReplaceHistoryEntry = replace
+        };
+        navigationManager.NavigateTo(url, options);
+    }
+}

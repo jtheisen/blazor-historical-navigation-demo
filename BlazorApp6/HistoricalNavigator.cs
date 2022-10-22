0e50f74b73b70aca66f8739aa185b10b8dcd55ac
jtheisen <jens.theisen@github.com>
2022-10-22 18:05:31 +0200

    Move towards two pages

--- BlazorApp6/HistoricalNavigator.cs
+++ BlazorApp6/HistoricalNavigator.cs
@@ -1,54 +1,83 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.JSInterop;
 using System.ComponentModel;
+using RouteData = Microsoft.AspNetCore.Components.RouteData;
 
 namespace BlazorApp6;
+
+public struct HistoryState
+{
+    public RouteData routeData;
+}
 
 public class HistoricalNavigator : INotifyPropertyChanged
 {
     private readonly IJSRuntime js;
     private readonly NavigationManager navigationManager;
 
-    Int32 currentIndex = 0;
+    List<RouteData?> history = new List<RouteData?>();
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
-    public Int32 CurrentPageIndex => currentIndex;
+    public Int32 CurrentHistoryIndex
+    {
+        get
+        {
+            if (Int32.TryParse(navigationManager.HistoryEntryState, out var index))
+            {
+                return index;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    public void EnsureHistorySlot(Int32 i)
+    {
+        while (history.Count <= i)
+        {
+            history.Add(default);
+        }
+    }
+
+    public RouteData SetCurrentRouteData(RouteData routeData)
+    {
+        var i = CurrentHistoryIndex;
+
+        EnsureHistorySlot(i);
+
+        history[i] = routeData;
+
+        return routeData;
+    }
+
+    public RouteData? GetPreviousRoutData()
+    {
+        var i = CurrentHistoryIndex - 1;
+
+        if (i < 0 || i >= history.Count) return null;
+
+        return history[i];
+    }
 
     public HistoricalNavigator(IJSRuntime js, NavigationManager navigationManager)
     {
         this.js = js;
         this.navigationManager = navigationManager;
-
-        navigationManager.RegisterLocationChangingHandler(HandleLocationChanging);
-        navigationManager.LocationChanged += HandleLocationChanged;
-    }
-
-    private ValueTask HandleLocationChanging(LocationChangingContext ctx)
-    {
-        if (Int32.TryParse(ctx.HistoryEntryState, out currentIndex))
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(""));
-        }
-
-        return ValueTask.CompletedTask;
-    }
-
-    private void HandleLocationChanged(Object? _, LocationChangedEventArgs e)
-    {
-        if (Int32.TryParse(e.HistoryEntryState, out currentIndex))
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(""));
-        }
     }
 
     [JSInvokable]
     public void Navigate(String url, Boolean replace)
     {
+        var i = CurrentHistoryIndex;
+
         var options = new NavigationOptions
         {
-            HistoryEntryState = (replace ? currentIndex : currentIndex + 1).ToString(),
+            HistoryEntryState = (replace ? i : i + 1).ToString(),
             ReplaceHistoryEntry = replace
         };
         navigationManager.NavigateTo(url, options);
